36732bb399197feb39a93e3691ab2ef0480c0195
Alex Day <alexdaygjj@gmail.com>
2025-01-16 20:04:56 -0700

    Implement simple test

---
+++ test.py
@@ -0,0 +1,15 @@
+import pyautogui as gui
+import time
+
+gui.moveTo(2120, 435)
+time.sleep(5)
+gui.moveTo(2411, 435)
+time.sleep(5)
+gui.moveTo(2700, 435)
+time.sleep(5)
+gui.moveTo(2980, 435)
+time.sleep(5)
+gui.moveTo(3275, 435)
+time.sleep(5)
+gui.scroll(-360)
+time.sleep(5)
